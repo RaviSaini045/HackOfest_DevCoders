b9e8f8c8bf1490d7fabdc09fca4aceb13c29e1ba
tdgadwani <tusharg.ug21.cs@nitp.ac.in>
2024-04-07 13:37:59 +0530

    fixed typos

--- Backend/src/controllers/resetPassword.controller.js
+++ Backend/src/controllers/resetPassword.controller.js
@@ -1,4 +1,4 @@
-import { asynchandler } from "../utils/asynchandler.js";
+import { asynchandler } from "../utils/Asynchandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
